8662364ca35499dde44d0a1a525de0a71b821c63
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-07 06:24:01 +0530

    Adding File

---
+++ 01. Getting familiar with python.py
@@ -0,0 +1,31 @@
+# Input
+a= input("Enter the value:- ")# Don't forget to give input each time this code is executed 
+b= int(input("Only Integer else there will be an error:- "))
+c= float(input("Only Float else there will be an error:- "))
+
+# Output
+print(a)
+print(b+ c)
+print(5+2)
+print("Python")
+print(7,8.12,"Python ", sep= "-", end= "$")
+
+# Multiple Output
+print(a,7,8,6+5)
+
+# Comments - lines that will not be executed
+
+# Single- Line Comment
+
+''' 
+Multi- Line Comments used to comment multiple line,
+instead of using # each time. 
+'''
+"""
+We can also use Double- Quoted- 
+Three- inverted- commas
+"""
+
+# Escape Sequence Character- For functions that normally cannot be used in strings
+
+print("This is for escape sequence\t \" We can use\" symbol too\n Escape Sequence")
