2e3f365c5e78c62566ade2301691f1e92d8028c7
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-08 07:20:27 +0530

    Updating Content

---
+++ 10. Functions.py
@@ -0,0 +1,7 @@
+'''
+Function:- A function is a block of code that performs a specific task whenever it is called.
+Two Types :- 
+Built-in Functions:- The functions that are pre-defined and pre-coded in python.
+User Definded Functions:- The functions we create to perform specific tasks as per our needs.
+Calling a function:- We call a function by the function name, followed by parameters (if any) inside the parenthesis.
+'''
