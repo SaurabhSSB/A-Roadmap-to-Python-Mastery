bbfceb3085c5a207861ca95786813966850e9053
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-17 00:31:03 +0530

    Updating Content

--- 10. Functions.py
+++ 10. Functions.py
@@ -5,6 +5,7 @@ Built-in Functions:- The functions that are pre-defined and pre-coded in python.
 User Definded Functions:- The functions we create to perform specific tasks as per our needs.
 Calling a function:- We call a function by the function name, followed by parameters (if any) inside the parenthesis.
 '''
+
 def Out():
   print("No Parameters Required")
 def Add(a, b): # Here a and b are Parameters and Arguments are passed to these Parameters
@@ -20,6 +21,7 @@ def Small(a, b):
     print("Second Number is Smaller")
   else:
     print("First Number is Smaller")
+
 a= 12
 b= 89
 Out()
@@ -29,6 +31,7 @@ c= 82
 d= 7
 Small(c, d)
 Add(c, d)
+
 '''
 Arguments in Python are of four Types:
 Default Arguments
@@ -36,21 +39,26 @@ Keyword Arguments
 Variable Length Arguments
 Required Arguments
 '''
+
 '''
 Default Argument:- We provide a default value while creating a function.
 '''
+
 def Add(a= 5, b= 7): 
   print("Sum is:- ",a+b)
 Add()
+
 '''
 Keyword Arguemnts:- Arguments with key = value.
 By this the interpreter recognizes the arguments by the parameter name.
 So the order in which the arguments are passed is not relevant.
 '''
+
 def Multiplication(a, b, c):
     print("a= ", a, "b= ", b, "c= ", c)
     print("Product is ", a*b*c)
 Multiplication(c= 12, b= 15, a= 11)
+
 '''
  Variable-length arguments:- Used to pass more arguments than defined in the actual function.
 '''
