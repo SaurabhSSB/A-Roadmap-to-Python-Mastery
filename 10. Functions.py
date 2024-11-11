b3ed513769ef2b8a3635182ea667db60ac03b8e4
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-12 00:20:50 +0530

    Updating Content

--- 10. Functions.py
+++ 10. Functions.py
@@ -5,7 +5,6 @@ Built-in Functions:- The functions that are pre-defined and pre-coded in python.
 User Definded Functions:- The functions we create to perform specific tasks as per our needs.
 Calling a function:- We call a function by the function name, followed by parameters (if any) inside the parenthesis.
 '''
-
 def Out():
   print("No Parameters Required")
 def Add(a, b): # Here a and b are Parameters and Arguments are passed to these Parameters
@@ -16,6 +15,10 @@ def Bigger(a, b):
     print("First Number is Bigger")
   else:
     print("Second number is Bigger")
+
+
+
+
 def Small(a, b):
   if(a>b):
     print("Second Number is Smaller")
