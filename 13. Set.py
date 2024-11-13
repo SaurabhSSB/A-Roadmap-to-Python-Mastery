b51132b47e6001314d7aa6e64aea95c4282a561d
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-14 02:47:29 +0530

    Updating Content

--- 13. Set.py
+++ 13. Set.py
@@ -13,3 +13,29 @@ for i in b:
 print("end")
 
 # Manipulation
+
+a1= {23, 22, 45, 56}
+a2= {1, 2, 3, 4, 5, 6}
+a1.update(a2)
+a1.difference_update(a2)
+a3=a1.difference(a2)
+print(a3)
+print(a1)
+print(a1.union(a2),a1.intersection(a2))
+a1.update(a2)
+a3= a1.symmetric_difference(a2)
+a1.intersection_update(a2)
+print(a1)
+print(a3)
+
+'''
+union()
+update()
+intersection
+intersectin_update()
+symmetric_difference
+symmetric_difference_update()
+difference()
+difference_update()
+'''
+
