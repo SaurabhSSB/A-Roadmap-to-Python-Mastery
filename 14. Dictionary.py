6c4865c8b8b279ed700267f39a2be1b2da72b4df
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-14 03:48:50 +0530

    Updating Content

--- 14. Dictionary.py
+++ 14. Dictionary.py
@@ -12,6 +12,19 @@ print(a.get("Ram"))# Does not give error in case of controversy
 print(a.keys())
 print(a.values())
 print(a.items())
+a.pop("Shyam")
+a.popitem()
+del a["Ram"]
 
-for a,b in a.items():
-    print(f"The way to print {a} along with the right {b} consistency is key.")
+for x,y in a.items():
+    print(f"The way to print {x} along with the right {y} consistency is key.")
+
+b={
+    "Hari":"Task",
+    "Mohan":"Analyst"
+}
+a.update(b)
+print(a)
+b.clear()
+print(b)
+del b
