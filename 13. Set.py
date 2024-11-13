4573d60a143d162d0ac2986469b6320dc6423cc8
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-14 01:31:52 +0530

    Adding Content

---
+++ 13. Set.py
@@ -0,0 +1,15 @@
+# Set is an unordered collection of data, it stored multiple entries in a single variable.
+# Seperated by Commas and enclosed within {}
+# Sets are immutable. 
+# Sets do not contain Duplicate Items.
+
+a=set()
+b={2,3,4,5,2,"Tenacious",True,2.0}
+print(type(a))
+print(b)
+
+for i in b:
+    print(i,end=",")
+print("end")
+
+# Manipulation
