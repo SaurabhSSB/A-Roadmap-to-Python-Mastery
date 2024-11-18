e877ccf0342203ce4ce2a8f413602661257506fb
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-18 19:18:08 +0530

    Adding Content

---
+++ 18. File Operations.py
@@ -0,0 +1,15 @@
+with open("ABC.txt","a") as a:
+    a.write("\nFinally able to do operation on file. Thanks Python!")
+
+a= open("ABC.txt","r")
+r= a.read()
+print(r)
+a.close()
+
+# r mode- Read Default
+# w mode- Write
+# a mode- Append
+# x mode- Create
+# t mode- Text Default
+# b mode- Binary Jpg, Image, Pdf, exe
+
