3dec324f6304dbd33733a3210b8e3066bb0299ad
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-22 10:17:09 +0530

    Updating Content

--- 18. File Operations.py
+++ 18. File Operations.py
@@ -2,10 +2,14 @@ with open("ABC.txt","a") as a:
     a.write("\nFinally able to do operation on file. Thanks Python!")
 
 a= open("ABC.txt","r")
+
+
+
+
+
 r= a.read()
 print(r)
 a.close()
-
 # r mode- Read Default
 # w mode- Write
 # a mode- Append
