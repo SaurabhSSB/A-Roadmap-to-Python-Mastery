537601acf539169bce730c528eb0670cc74396e8
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-01 19:38:15 +0530

    Updating Content

--- 18. File Operations.py
+++ 18. File Operations.py
@@ -6,13 +6,15 @@ r= a.read()
 print(r)
 a.close()
 
+
+
+
 # r mode- Read Default
 # w mode- Write
 # a mode- Append
 # x mode- Create
 # t mode- Text Default
 # b mode- Binary Jpg, Image, Pdf, exe
-
 f= open("ABC.txt","r")
 while True:
     line= f.readline()
