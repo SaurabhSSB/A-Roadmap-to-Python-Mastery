58b0471c16ced071d2e680b8c3d8e5e096454dd3
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-02 22:17:13 +0530

    Updating Content

--- Afunc.py
+++ Afunc.py
@@ -2,12 +2,7 @@ def Multiplication(a, b, c):
     print("a= ", a, "b= ", b, "c= ", c)
     print("Product is ", a*b*c)
 
-
-
-
-
-
-
 if __name__=="__main__":
     Multiplication(2,3,4)
+
 print("This is:- ",__name__)
