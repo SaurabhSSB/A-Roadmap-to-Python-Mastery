816a431c3ba3b3861db6a146ba6dbdeeb4a554a1
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-03 04:19:45 +0530

    Updating Content

--- 19. Map.py
+++ 19. Map.py
@@ -1,6 +1,17 @@
+# Map- Value Takes Argument
+
 def Increment(x):
     return(x+1)
 
-a={12, 10, 11, 14, 19, 21, 42}
+a=[12, 10, 11, 14, 19, 21, 42]
 b=list(map(Increment, a))
 print(b)
+
+c=list(map(lambda x:x+1,a))
+print(c)
+
+# Filter- True or False Takes Predicate
+d=list(filter(lambda x: x<21, c))
+print(d)
+
+# Reduce
