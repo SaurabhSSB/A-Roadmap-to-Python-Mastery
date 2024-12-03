deeb91e9790d90c8b1c54d7bc138c3014d19ffd0
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-04 03:52:51 +0530

    Modifying Content

--- 20. OOP.py
+++ 20. OOP.py
@@ -3,6 +3,7 @@ Object- Oriented Programming in Python is to use classes and objects to represen
 Object- Oriented Programming is used to map real world entities.
 Entry- Class/ Blue- Print/ Template- Properties and methods that an object in class will have
 Students- Object/ Entity- Instance of Class- contains it's own data and methods
+Python follows PascalCase for class names (first letter capitalized).
 '''
 class Student:
     roll_no= 10
@@ -15,3 +16,10 @@ x= Student()
 x. name= "Nittin"
 x. roll_no= 16
 x.info()
+
+a= [10, 14, 16, 27, 36, 49]
+print(dir(a))
+print(a.__class__)
+
+print(x.__dict__)
+print(help(Student))
