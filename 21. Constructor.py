38178b75e7b6546593cc0db6a41f8d37dc8a13c4
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-04 03:53:02 +0530

    Updating Content

--- 21. Constructor.py
+++ 21. Constructor.py
@@ -12,3 +12,21 @@ x= Student( 10, "Saurabh Singh Bhandari", "BTech")
 y= Student( 16, "Nittin", "BTech")
 x.info()
 y.info()
+
+# Instance & Class Variable
+
+class Student:
+    School= "Kendriya Vidyalaya" # Class Variable
+    Student_no= 0 
+    def __init__ (self, roll_no,name):
+        self.roll_no= roll_no
+        self.name= name
+        self._class= 12 # Instance Varaible
+        Student.Student_no+= 1
+    def show(self):
+        print(f"{self.name} Roll No.{self.roll_no} School {Student.School} once studied in {self._class} class in {self.School}")
+
+a= Student(10, "Saurabh Singh Bhandari")
+print(Student.School)
+a.show()
+print(f"Total Student enrolled= {Student.Student_no}")
