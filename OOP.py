adae259388bbd8e15779e16c0e51a3fa09a83a3d
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-03 09:15:26 +0530

    Adding Content

---
+++ OOP.py
@@ -0,0 +1,6 @@
+'''
+Object- Oriented Programming in Python is to use classes and objects to represent real- world concepts and entities.
+Object- Oriented Programming is used to map real world entities.
+Entry- Class/ Blue- Print/ Template- Properties and methods that an object in class will have
+Students- Object/ Entity- Instance of Class- contains it's own data and methods
+'''
