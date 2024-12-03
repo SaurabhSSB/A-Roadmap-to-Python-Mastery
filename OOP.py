30061c7f712b1a383b6ca7a0c00ad9769c796e90
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-03 09:27:44 +0530

    Updating Content

--- OOP.py
+++ OOP.py
@@ -4,3 +4,14 @@ Object- Oriented Programming is used to map real world entities.
 Entry- Class/ Blue- Print/ Template- Properties and methods that an object in class will have
 Students- Object/ Entity- Instance of Class- contains it's own data and methods
 '''
+class Student:
+    roll_no= 10
+    name= "Saurabh Singh Bhandari"
+    graduation= "BTech"
+    def info(self):
+        print(f"Roll No. {self.roll_no} name is {self.name} he has a {self.graduation} degree")
+
+x= Student()
+x. name= "Nittin"
+x. roll_no= 16
+x.info()
